6818ff8df17c77bb2e621058b59446b958ec5edb
kaliscandinavia <78321094+kaliscandinavia@users.noreply.github.com>
2021-03-20 17:54:10 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+var next = document.getElementById("nextbtn");
+var back = document.getElementById("backbtn");
+var image = document.getElementById("image");
+var description = document.getElementById("description");
+var currentImage = 0;
+
+//image array
+var images = [
+  {
+link: "bg0.png",
+text: "From andviksfjellet."
+},
+{
+  link: "bg2.png",
+  text: "Torgalvmenningen"
+},
+{
+link: "bg1.png",
+text: "Smitt & smau"
+},
+{
+link: "nosy.png",
+text: "A habitant"
+}
+];
+
+//Next event handler
+next.addEventListener("click",function (){
+  currentImage = currentImage + 1;
+  if (currentImage === images.length){
+currentImage = 0;
+}
+image.src = images[currentImage].link;
+discription.textContent = images[currentImage].text;
+});
+
+//Back event handler
+back.addEventListener("click",function () {
+  currentImage = currentImage -1;
+  if (currentImage === -1){
+    currentImage = image.length -1;
+  }
+  image.src = images[currentImage].link;
+  description.textContent = images[currentImage].text;
+
+
+});
